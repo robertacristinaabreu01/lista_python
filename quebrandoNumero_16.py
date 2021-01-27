afd314fbb0d0aff792d9a7013ad216b2f870d554
robertacristinaabreu01 <robertacristinaabreu01@gmail.com>
2021-01-27 16:14:44 -0300

    exerci_27Jan

---
+++ quebrandoNumero_16.py
@@ -0,0 +1,4 @@
+#crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+#digite um número:6.127 sendo 6 a parte inteira
+
+num =  float(input("Digite um número "))
