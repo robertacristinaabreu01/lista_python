fc5d144da788fe3b53ee61edb3a4b6ca8227ada7
robertacristinaabreu01 <robertacristinaabreu01@gmail.com>
2021-01-27 16:38:22 -0300

    atualizar

--- catetos_hipotenusa_17.py
+++ catetos_hipotenusa_17.py
@@ -1,2 +1,12 @@
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo
 # retângulo. Calcule e mostre o comprimento da hipotenusa.
+#h2 = ca2 + co2
+#hipotenusa = co **2 +ca **2 )**(1/2)
+#hipotenusa = co **2 +ca **2 )**(1/2)
+
+import math
+oposto = float(input("Digite o comprimento do cateto oposto: "))
+adja = float(input("Digite o comprimento do cateto adjacente: "))
+hipotenusa = math.hypot(oposto,adja)
+print("A hipotenusa será: {:.2f}".format(hipotenusa))
+
--- quebrandoNumero_16.py
+++ quebrandoNumero_16.py
@@ -1,4 +1,8 @@
 #crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
 #digite um número:6.127 sendo 6 a parte inteira
-
-num =  float(input("Digite um número "))
+import math
+#num = float(input("Digite um número :"))
+#print("A parte inteira do número {}  será: {}".format(num,trunc(num)))
+#ou
+num = float(input("Digite um número :"))
+print("A parte inteira do número {}  será: {}".format(num,math.trunc(num)))
