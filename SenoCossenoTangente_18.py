c4c7ecc432aab45164b431d43f99d1bab815a573
robertacristinaabreu01 <robertacristinaabreu01@gmail.com>
2021-01-27 17:23:38 -0300

    atualizar 2

--- SenoCossenoTangente_18.py
+++ SenoCossenoTangente_18.py
@@ -1,1 +1,12 @@
-#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno
+# e tangente desse ângulo.
+import math
+angulo = float(input("Digite o ângulo: "))
+
+cos = math.cos(math.radians(angulo))
+sen = math.sin(math.radians(angulo))
+tang = math.tan(math.radians(angulo))
+
+print("Para o ângulo {} será {:.2f} cos, {:.2f} seno e {:.2f} tangente".format(angulo, cos, sen, tang))
+
+
